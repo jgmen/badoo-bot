c76134534b6ea25e6b6bac05c27868a7a51dcf45
joaogmendes <joaogmendess19@gmail.com>
2025-06-14 20:58:48 -0300

    feat: intercept and capture request signature with Playwright

---
+++ src/core/browser_manager.py
@@ -0,0 +1,38 @@
+from playwright.async_api import ChromiumBrowserContext, async_playwright
+
+
+class BrowserManager:
+    __context: ChromiumBrowserContext
+    __instance = None
+    __playwright = None
+    __perfil_path = ".config/chromium"
+    __context_config = {
+        "headless": False,
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+        "viewport": {"width": 1280, "height": 800},
+        "locale": "pt-BR",
+        "timezone_id": "America/Sao_Paulo",
+        "args": ["--start-maximized", "--disable-blink-features=AutomationControlled"],
+    }
+
+    @classmethod
+    async def get_instance(cls):
+        if cls.__instance is None:
+            cls.__instance = await cls._create_browser_instance()
+        return cls.__instance
+
+    @classmethod
+    async def _create_browser_instance(cls):
+        cls.__playwright = await async_playwright().start()
+        context = await cls.__playwright.chromium.launch_persistent_context(
+            user_data_dir=cls.__perfil_path, **cls.__context_config
+        )
+        cls.__context = context
+        return cls.__context
+
+    @classmethod
+    async def close(cls):
+        if cls.__context:
+            await cls.__context.close()
+        if cls.__playwright:
+            await cls.__playwright.stop()
--- src/main.py
+++ src/main.py
@@ -1,16 +1,87 @@
-from playwright.sync_api import sync_playwright
+import asyncio
+from typing import Dict, Any
+from core.browser_manager import BrowserManager
+from dataclasses import dataclass
+from playwright.async_api import Request, Route
 
-with sync_playwright() as p:
-    perfil_path = ".config/chromium"
-    context = p.chromium.launch_persistent_context(
-        user_data_dir=perfil_path,
-        headless=False,
-    )
 
-    page = context.pages[0] if context.pages else context.new_page()
+@dataclass
+class RequestSignature:
+    url: str
+    headers: Dict[str, str]
+    post_data: Dict[str, Any]
 
-    page.goto("https://badoo.com/encounters")
-    page.wait_for_load_state("networkidle")
 
-    input("Pressione Enter para fechar o navegador...")
-    context.close()
+async def intercept_request_signature(
+    signature_url: str, trigger_url: str
+) -> RequestSignature:
+    """
+    Intercepts the first request matching the given URL using Playwright
+    and returns its headers and body payload.
+    """
+    browser = BrowserManager()
+    context = await browser.get_instance()
+    page = await context.new_page()
+
+    result: Dict[str, RequestSignature] = {}
+
+    lock = asyncio.Lock()
+    captured = {"done": False}
+
+    # Set route handler to intercept matching API requests
+    async def intercept_api_request(route: Route):
+        """
+        Callback helper function to intercept the first api url
+        """
+        request: Request = route.request
+
+        if request.url == signature_url and not captured["done"]:
+            async with lock:
+                post_data = request.post_data_json
+                headers = await request.all_headers()
+                if post_data and headers:
+                    result["signature"] = RequestSignature(
+                        url=request.url,
+                        headers=headers,
+                        post_data=post_data,
+                    )
+                    captured["done"] = True
+        await route.continue_()
+
+    await page.route("**/mwebapi.phtml*", intercept_api_request)
+
+    # Acessing content
+    await page.goto(trigger_url)
+
+    # Waiting full load
+    await page.wait_for_load_state("load")
+
+    for _ in range(10):
+        if result.get("signature"):
+            break
+        await asyncio.sleep(0.5)
+    else:
+        raise Exception(
+            f"Timeout: Could not capture the request for URL: {signature_url} within the timeout period."
+        )
+
+    # Close browser
+    await browser.close()
+
+    if "signature" in result:
+        return result["signature"]
+    else:
+        raise Exception(
+            f"No request signature was intercepted for URL: {signature_url}"
+        )
+
+
+async def main():
+    signature_url = "https://badoo.com/mwebapi.phtml?SERVER_GET_USER_LIST"
+    trigger_url = "https://badoo.com/connections"
+    signature = await intercept_request_signature(signature_url, trigger_url)
+    print(signature)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
